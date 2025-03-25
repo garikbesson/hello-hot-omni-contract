0d600fbd619e6c1b9c961350c0e82f676be092d1
garikbesson <garikbesson@gmail.com>
2025-03-25 13:32:50 +0000

    deposit, withdraw tokens

---
+++ README.md
@@ -0,0 +1,36 @@
+# hello-hot-omni-contract
+
+The example of a near smart contract that interacts with Hot Omni smart contract (depositing, checking balances, withdrawing, and transferring functionality).
+
+## How to Build Locally?
+
+Install [`cargo-near`](https://github.com/near/cargo-near) and run:
+
+```bash
+cargo near build
+```
+
+## How to Test Locally?
+
+```bash
+cargo test
+```
+
+## How to Deploy?
+
+To deploy manually, install [`cargo-near`](https://github.com/near/cargo-near) and run:
+
+```bash
+cargo near deploy build-reproducible-wasm <account-id>
+```
+
+## Useful Links
+
+- [cargo-near](https://github.com/near/cargo-near) - NEAR smart contract development toolkit for Rust
+- [near CLI](https://near.cli.rs) - Interact with NEAR blockchain from command line
+- [NEAR Rust SDK Documentation](https://docs.near.org/sdk/rust/introduction)
+- [NEAR Documentation](https://docs.near.org)
+- [NEAR StackOverflow](https://stackoverflow.com/questions/tagged/nearprotocol)
+- [NEAR Discord](https://near.chat)
+- [NEAR Telegram Developers Community Group](https://t.me/neardev)
+- NEAR DevHub: [Telegram](https://t.me/neardevhub), [Twitter](https://twitter.com/neardevhub)
---
+++ src/lib.rs
@@ -0,0 +1,130 @@
+use bs58;
+use near_sdk::{
+    env, ext_contract, json_types::U128, log, near, serde_json, Gas, NearToken, Promise,
+    PromiseError,
+};
+use serde_json::json;
+use sha2::{Digest, Sha256};
+
+const HOT_OMNI_CONTRACT: &str = "v1-1.omni.hot.tg";
+
+#[ext_contract(usdt_contract)]
+trait UsdtContract {
+    fn ft_transfer_call(&self, amount: String, receiver_id: String, msg: String);
+}
+
+#[ext_contract(hot_omni_contract)]
+trait HotOmniContract {
+    fn get_balance(&self, account_id: String);
+    fn omni_transfer(&self, account_id: String);
+    fn withdraw_on_near(&self, account_id: String, token_id: u8, amount: String);
+}
+
+#[near(contract_state)]
+pub struct Contract {
+    greeting: String,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            greeting: "Hello".to_string(),
+        }
+    }
+}
+
+fn get_omni_address(account_id: String) -> String {
+    return bs58::encode(Sha256::digest(account_id.as_bytes())).into_string();
+}
+
+#[near]
+impl Contract {
+    pub fn deposit(&mut self, amount: String) -> Promise {
+        usdt_contract::ext("usdt.tether-token.near".parse().unwrap())
+            .with_static_gas(Gas::from_tgas(80))
+            .with_attached_deposit(NearToken::from_yoctonear(1))
+            .ft_transfer_call(
+                amount,
+                HOT_OMNI_CONTRACT.to_string(),
+                get_omni_address(env::current_account_id().to_string()),
+            )
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(Gas::from_tgas(5))
+                    .deposit_callback(),
+            )
+    }
+
+    #[private]
+    pub fn deposit_callback(
+        &mut self,
+        #[callback_result] call_result: Result<String, PromiseError>,
+    ) -> String {
+        if call_result.is_err() {
+            log!("There was an error depositing on HOT Omni Contract");
+            return "".to_string();
+        }
+
+        let deposited_amount: String = call_result.unwrap();
+        deposited_amount
+    }
+
+    pub fn get_omni_balance(&self, token_id: String) -> Promise {
+        let account_id = env::current_account_id();
+        hot_omni_contract::ext("v1-1.omni.hot.tg".parse().unwrap())
+            .with_static_gas(Gas::from_tgas(5))
+            .get_balance(get_omni_address(account_id.to_string()))
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(Gas::from_tgas(5))
+                    .get_omni_balance_callback(token_id),
+            )
+    }
+
+    #[private]
+    pub fn get_omni_balance_callback(
+        &self,
+        token_id: String,
+        #[callback_result] call_result: Result<serde_json::Value, PromiseError>,
+    ) -> String {
+        if call_result.is_err() {
+            log!("There was an error querying user's balances on HOT Omni Contract");
+            return "".to_string();
+        }
+
+        let balances = call_result.unwrap();
+        let balance = balances.get(&token_id).unwrap().to_string();
+        balance
+    }
+
+    pub fn withdraw(&self, amount: U128, token_id: u8) -> Promise {
+        let account_id = env::current_account_id();
+        hot_omni_contract::ext("v1-1.omni.hot.tg".parse().unwrap())
+            .with_static_gas(Gas::from_tgas(80))
+            .with_attached_deposit(NearToken::from_yoctonear(1))
+            .withdraw_on_near(
+                get_omni_address(account_id.to_string()),
+                token_id,
+                amount.0.to_string(),
+            )
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(Gas::from_tgas(5))
+                    .withdraw_callback(),
+            )
+    }
+
+    #[private]
+    pub fn withdraw_callback(
+        &self,
+        #[callback_result] call_result: Result<(), PromiseError>,
+    ) -> bool {
+        if call_result.is_err() {
+            log!("There was an error withdrawing from HOT Omni Contract");
+            false
+        } else {
+            env::log_str("withdrawing was successful!");
+            true
+        }
+    }
+}
